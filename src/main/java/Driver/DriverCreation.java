541cebbc8c6a6136f8e04eb268844329b697ba52
Tatevar <42369756+Tatevar@users.noreply.github.com>
2021-11-01 18:44:39 +0400

    BasePages creation

---
+++ src/main/java/Driver/DriverCreation.java
@@ -0,0 +1,53 @@
+package Driver;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.ie.InternetExplorerDriver;
+import properties.PropertyReader;
+/*
+    Patten Singleton
+ */
+
+public class DriverCreation {
+    private static ThreadLocal<WebDriver> driver = new ThreadLocal<>();
+
+    public static void setDriver(String browserType) {
+        if (driver.get() == null) {
+            createDriver(browserType);
+        }
+    }
+
+    private static void createDriver(String browserType) {
+        switch (browserType) {
+            case "Chrome":
+                WebDriverManager.chromedriver().setup();
+                ChromeOptions chromeOptions = new ChromeOptions();
+                chromeOptions.addArguments(PropertyReader.getProperties().getProperty("chrome.driver.options").split(";"));
+                driver.set(new ChromeDriver(chromeOptions));
+                break;
+            case "Mozilla":
+                WebDriverManager.firefoxdriver().setup();
+                driver.set(new FirefoxDriver());
+                break;
+            case "IE":
+                WebDriverManager.iedriver().setup();
+                driver.set(new InternetExplorerDriver());
+                break;
+        }
+    }
+
+    public static WebDriver getDriver() {
+        return driver.get();
+    }
+
+    public static void quitDriver() {
+        if (driver.get() != null) {
+            driver.get().close();
+            driver.get().quit();
+            driver.remove();
+        }
+    }
+}
